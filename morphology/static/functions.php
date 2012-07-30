1ea546fda7cec2168ec6c7f4691879c98cbd6fd7
Asheesh Laroia <asheesh@asheesh.org>
2012-07-29 18:43:14 -0700

    Add a snippet to disable the admin bar (and explain why)

--- morphology/static/functions.php
+++ morphology/static/functions.php
@@ -2,4 +2,10 @@
 /* Permit widgets to land in our sidebar */
 if ( function_exists('register_sidebar') )
     register_sidebar();
+
+/* Disallow the top admin bar from showing up.
+   This is only disallowed because the theme does not seem
+   to properly render the admin bar. I'd appreciate if someone could
+   try removing the add_filter() call and try the theme. */
+add_filter( 'show_admin_bar', '__return_false' );
 ?>
