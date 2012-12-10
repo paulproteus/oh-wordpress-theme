4999616ce6801a2bc4eccdb8948ca8867a195055
Asheesh Laroia <asheesh@asheesh.org>
2012-12-09 22:30:46 -0500

    Add support for post featured images, and use that as Facebook open graph data

--- morphology/static/functions.php
+++ morphology/static/functions.php
@@ -8,4 +8,9 @@ if ( function_exists('register_sidebar') )
    to properly render the admin bar. I'd appreciate if someone could
    try removing the add_filter() call and try the theme. */
 add_filter( 'show_admin_bar', '__return_false' );
+/* Permit the theme to accept "Featured images." This is used by the theme
+   to support Facebook's og:image metadata, which lets us control which images
+   are displayed. */
+add_theme_support( 'post-thumbnails' );
+set_post_thumbnail_size( 576, 9999 );
 ?>
